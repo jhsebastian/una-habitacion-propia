34ce4d338de6493ab624e7e1d091ae27f7bc5fa1
Sebastian Martinez Soto <sebastianmartinezsoto@MacBook-Pro-de-Sebastian.local>
2021-08-05 21:28:43 -0500

    add images for the quienes somos page

--- src/pages/QuienesSomos/QuienesSomos.jsx
+++ src/pages/QuienesSomos/QuienesSomos.jsx
@@ -1,13 +1,22 @@
 import Header from "../../commons/Header/Header";
 import Footer from "../../commons/Footer/Footer";
+import imagen1 from "../../assets/1.png";
+import imagen2 from "../../assets/2.png";
+import imagen3 from "../../assets/3.png";
+import imagen4 from "../../assets/4.png";
+import "./QuienesSomos.css";
 
 const QuienesSomos = () => {
     return (
         <div>
             <Header></Header>
-            <h3>This is the Who We Are page, soon you'll fine information about 
-                the project and the people behind it.
-            </h3>
+            <h1>NUESTRO EQUIPO</h1>
+            <div className="quienesSomos-principal-container">
+                <img className="image-quienesSomos img-fluid" src={imagen1} alt="" />
+                <img className="image-quienesSomos img-fluid" src={imagen2} alt="" />
+                <img className="image-quienesSomos img-fluid" src={imagen3} alt="" />
+                <img className="image-quienesSomos img-fluid" src={imagen4} alt="" />
+            </div>
             <Footer></Footer>
         </div>
     )
