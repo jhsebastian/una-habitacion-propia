43fc534aeaeb1a799dccbe945c07e2267cdb059b
jhsebastian <jhsebastian12@gmail.com>
2021-09-20 21:21:28 -0500

    some changes on the home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,8 +25,7 @@ const Home = () => {
             <Banner />
             <Lottie options={{animationData: mujer, ...defaultOptions}} width={300} height={300} />
             <div className="temporal-container">
-                <h2>Hola, este sitio web está en construcción</h2>
-                <h3>pronto podrás encontrar el sitio web y disfrutar.</h3>
+                
             </div>
             <div className="container-modals">
                 <ModalComponent
