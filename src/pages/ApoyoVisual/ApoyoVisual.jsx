be3aad723fa015a2494beff7a963614c28858b66
jhsebas_ <jhsebastian12@gmail.com>
2022-01-22 00:29:39 -0500

    integrate spotify and change title on producto audiovisual page

--- src/pages/ApoyoVisual/ApoyoVisual.jsx
+++ src/pages/ApoyoVisual/ApoyoVisual.jsx
@@ -6,9 +6,9 @@ const ApoyoVisual = () => {
     return (
         <div>
             <Header />
-            <h1>Esta es la página de contenido visual, pronto encontrarás fotos y vídeos de 
-            nuestra experiencia.
+            <h1>producto audiovisual
             </h1>
+            <iframe src="https://open.spotify.com/embed/show/1hxVtMnnNNtgstmEp6qDnm?utm_source=generator" width="100%" height="232" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
             <Footer />
         </div>
     )
